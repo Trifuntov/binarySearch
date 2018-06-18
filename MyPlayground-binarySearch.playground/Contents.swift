d80ae2595d959f10b849a06e66077c60f9903f02
Igor Trifuntov <igor.trifuntov@gmail.com>
2018-06-19 00:55:34 +0700

    new

---
+++ MyPlayground-binarySearch.playground/Contents.swift
@@ -0,0 +1,41 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var array = [1,4,3,7,8,6,7,8,9,0]
+let srh = 4
+array = array.sorted()
+
+func binarySearch (array: [Int], value: Int) -> Int?{
+    
+    if !array.isEmpty{
+        var indexMin = array.startIndex
+        var indexMax = array.endIndex-1
+        
+        while indexMin <= indexMax{
+            
+            let indexMid = indexMin + indexMax
+            
+            if array[indexMid] == value{
+                return indexMid
+            }
+            
+            if value < array[indexMid]{
+                indexMax = indexMid-1
+            }else{
+                indexMin = indexMid+1
+            }
+        }
+    }
+    return nil
+}
+binarySearch(array: array, value: srh)
+
+
+
+
+
+
+
+
+
